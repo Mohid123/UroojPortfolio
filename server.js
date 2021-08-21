a62953ffe09be3ab4b1de4fd9a6e71f08fbb7069
Muhammad Mohid Dastgeer <muhammadmohid141@gmail.com>
2021-08-21 21:26:28 +0500

    SASDDSD

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/app'));
 
 app.get('/*', function(req,res) {
-	res.sendFile(path.join(__dirname+'/dist/myapp/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
